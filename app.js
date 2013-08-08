aecae91f7b7404b83c426ce2bec269ab28353a8d
Nathan Lintz <nlintz@gmail.com>
2013-08-08 09:18:14 -0700

    first commit for socket monsters!

---
+++ app.js
@@ -0,0 +1,63 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , game = require('./routes/game')
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/game', game.index)
+
+var server = http.createServer(app)
+
+var io = require('socket.io').listen(server)
+
+server.listen(5000)
+
+var usernames = {}
+
+io.sockets.on('connection', function(socket){
+	socket.on('sendchat', function(data){
+		io.sockets.emit('updatechat', socket.username, data);
+	});
+
+	socket.on('adduser', function(username){
+		socket.username = username;
+		usernames[username] = username;
+		socket.emit('updatechat', 'SERVER', 'you have connected');
+		socket.broadcast.emit('updatechat', 'SERVER', username+ ' has connected');
+		io.sockets.emit('updateusers', usernames);
+	});
+
+	socket.on('disconnect', function(){
+		delete usernames[socket.username];
+		io.sockets.emit('updateusers', usernames);
+		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+	});
+
+});
+
---
+++ public/javascripts/game.pde
@@ -0,0 +1,8 @@
+void setup(){
+	frameRate(30);
+
+}
+
+void draw(){
+	// console.log('hi/')
+}
